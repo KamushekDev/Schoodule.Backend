1c44a0c73347f6c11925f65f62a7c5b339e6be05
Kirill Ilin <kamushek.stone@gmail.com>
2021-05-17 11:20:04 +0300

    SS-43, migration

---
+++ Schoodule.DataAccess/Migrations/20210517081735_Lalka2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Schoodule.DataAccess.Migrations
+{
+    public partial class Lalka2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "weekday",
+                table: "classes",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "weekday",
+                table: "classes",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "smallint");
+        }
+    }
+}
